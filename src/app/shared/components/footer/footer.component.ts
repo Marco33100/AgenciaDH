5e97f3baf94069b3eb1f7969ea69b8087c622348
Marco <martinezramirezm82@gmail.com>
2024-11-26 09:19:10 -0600

    se agrego el chatbot

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,31 +1,14 @@
-import { Component, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
-import { isPlatformBrowser } from '@angular/common';
-
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements AfterViewInit {
+export class FooterComponent {
+  isChatbotVisible: boolean = false; 
 
   toggleChatbot(): void {
-    const chatbotContainer = document.getElementById('chatbot-container') as HTMLElement;
-    if (chatbotContainer.style.display === "none" || chatbotContainer.style.display === "") {
-      chatbotContainer.style.display = "block";
-    } else {
-      chatbotContainer.style.display = "none";
-    }
-  }
-
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) { }
-
-  ngAfterViewInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      const chatbotContainer = document.getElementById('chatbot-container') as HTMLElement;
-      if (chatbotContainer) {
-        chatbotContainer.style.display = "none";
-      }
-    }
+    this.isChatbotVisible = !this.isChatbotVisible; 
   }
 }
